6bfa67115bc8824f1c6b84973c9f8bdd78d72dc2
Gregor Monson <gregor.monson@fanduel.com>
2024-01-22 21:35:33 +0000

    Formatting fibonacci

--- projects/fibonacci/src/main.rs
+++ projects/fibonacci/src/main.rs
@@ -1,14 +1,13 @@
 extern crate num;
 
-use std::io;
-use std::time::{Instant};
 use num::BigUint;
-use num::{Zero, One};
+use num::{One, Zero};
+use std::io;
+use std::time::Instant;
 // use std::env;
 use std::collections::HashMap;
 
 fn fibbonaci_seed(first_value: BigUint, second_value: BigUint) -> HashMap<u32, BigUint> {
-
     let mut fibbonaci_hashmap = HashMap::<u32, BigUint>::new();
 
     fibbonaci_hashmap.insert(0, first_value);
@@ -17,8 +16,11 @@ fn fibbonaci_seed(first_value: BigUint, second_value: BigUint) -> HashMap<u32, B
     fibbonaci_hashmap
 }
 
-fn fibonacci_adder(mut fibbonaci_hashmap: HashMap<u32, BigUint>, requested_index: u32, count: u32) -> HashMap<u32, BigUint> {
-    
+fn fibonacci_adder(
+    mut fibbonaci_hashmap: HashMap<u32, BigUint>,
+    requested_index: u32,
+    count: u32,
+) -> HashMap<u32, BigUint> {
     let previous_fib_number = fibbonaci_hashmap.get(&(count)).unwrap().clone();
     let curent_fib_number = fibbonaci_hashmap.get(&(count + 1)).unwrap().clone();
     let next_fib_number = previous_fib_number.clone() + curent_fib_number.clone();
@@ -28,12 +30,7 @@ fn fibonacci_adder(mut fibbonaci_hashmap: HashMap<u32, BigUint>, requested_index
     fibbonaci_hashmap.insert(count + 2, next_fib_number);
 
     if count + 2 < requested_index {
-
-        fibbonaci_hashmap = fibonacci_adder(
-            fibbonaci_hashmap
-            , requested_index
-            , count + 1
-        )
+        fibbonaci_hashmap = fibonacci_adder(fibbonaci_hashmap, requested_index, count + 1)
     };
 
     fibbonaci_hashmap
@@ -43,12 +40,11 @@ fn fibonacci_call_index(index: u32) -> BigUint {
     let f0: BigUint = Zero::zero();
     let f1: BigUint = One::one();
 
-    let fibbonaci_hashmap = fibbonaci_seed( f0, f1 );
+    let fibbonaci_hashmap = fibbonaci_seed(f0, f1);
 
     let fibbonaci_hashmap = fibonacci_adder(fibbonaci_hashmap, index, 0);
 
     return fibbonaci_hashmap.get(&index).unwrap().clone();
-    
 }
 
 fn main() {
